cdeb0fdc3bcfa759e9b6772cf641dffdf6372b44
Angel <angel.villrey@gmail.com>
2026-01-31 20:57:13 -0600

    Arreglar puerto para Render

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "¡Hola! Esta es mi primera pagina web 😎"
+    return "Mi primera web en Internet 😎"
 
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
