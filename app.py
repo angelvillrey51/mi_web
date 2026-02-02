2fe423c2d3df42dac6b8b25a2221b6842d378027
Angel <angel.villrey@gmail.com>
2026-02-01 20:50:21 -0600

    no se que es esto

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import requests, os
 
 app = Flask(__name__)
 
-# CAMBIA por la IP REAL que te imprime el ESP32
 ESP32_URL = "http://192.168.1.11/abrir"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 @app.route("/abrir", methods=["POST"])
 def abrir_puerta():
     try:
-        r = requests.post(ESP32_URL, timeout=2)
+        requests.post(ESP32_URL, timeout=2)
         return jsonify({"status": "ok"}), 200
-    except Exception as e:
+    except:
         return jsonify({"error": "ESP32 no responde"}), 500
 
-
-# Ejemplo si ya tienes /datos
 @app.route("/datos", methods=["POST"])
 def datos():
     data = request.get_json()
     print("Datos recibidos:", data)
     return jsonify({"status": "recibido"}), 200
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
